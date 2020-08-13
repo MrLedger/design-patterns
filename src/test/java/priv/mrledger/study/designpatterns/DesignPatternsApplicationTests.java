492dae479a43c8dc41dcbd1ae37c031252c36848
lijuncheng <zero1165446650@outlook.com>
2020-08-13 08:32:45 +0800

    init

---
+++ src/test/java/priv/mrledger/study/designpatterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.mrledger.study.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
